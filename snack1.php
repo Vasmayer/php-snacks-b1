d79a63475d986eaf2aff44a60813885c3e30385b
Vasmayer <lealimarco90@gmail.com>
2022-02-25 15:09:03 +0100

    snack 1 start

---
+++ snack1.php
@@ -0,0 +1,21 @@
+<?php
+    /* Snack 1
+Passare come parametri GET name, mail e age e verificare 
+(cercando i metodi che non conosciamo nella documentazione)
+ che name sia più lungo di 3 caratteri, 
+ che mail contenga un punto e una chiocciola e che age sia un numero.
+  Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snack1</title>
+</head>
+<body>
+    
+</body>
+</html>
